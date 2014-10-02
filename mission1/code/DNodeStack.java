9931321f0f6b51b17b4c99aebb02a9efc04d01f3
Charles Jacquet <charles.jacquet7@gmail.com>
2014-10-02 15:37:35 +0200

    import

--- mission1/code/DNodeStack.java
+++ mission1/code/DNodeStack.java
@@ -2,58 +2,43 @@ public class DNodeStack<E> implements Stack<E>
 {
 	protected DNode<E> top;
 	protected int size;
-	
 	// Contruction d'une pile vide
-	public DNodeStack()
-	{
+	public DNodeStack(){
 		top = null;
 		size = 0;
 	}
-
-	public int size()
-	{
+	public int size(){
 		return size;
 	}
-
-	public boolean isEmpty()
-	{
+	public boolean isEmpty(){
 		if (top == null) return true;
 		return false;
 	}
-	
 	// Rajoute un element dans la pile
-	public void push(E dNode)
-	{
+	public void push(E dNode){
 		DNode<E> v = new DNode<E>(dNode, top);
 		top = v;
 		size++;
 	}
-	
 	// Renvoie le premiere element de la pile
-	public E top() throws EmptyStackException
-	{
+	public E top() throws EmptyStackException{
 		if (isEmpty()) throw new EmptyStackException("Stack is empty.");
 		return top.getElement();
 	}
-
 	// Enleve le premier element de la pile 
-	public E pop() throws EmptyStackException
-	{
+	public E pop() throws EmptyStackException{
 		if (isEmpty()) throw new EmptyStackException("Stack is empty.");
 		E temp = top.getElement();
 		top = top.getNext();
 		size--;
 		return temp;
 	}
-	
 	// Affiche le pile sous forme de string
-	public String toString()
-	{
+	public String toString(){
 		String out = "";
 		DNode<E> v = top;
 		if(v == null) return (out);
-		while(v.getNext() != null)
-		{
+		while(v.getNext() != null){
 			out += (v.getElement().toString() +" ");
 			v = v.getNext();
 		}
--- mission1/code/PSCalculatorAPP_test.java
+++ mission1/code/PSCalculatorAPP_test.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 public class PSCalculatorAPP_test extends PSCalculator_test {
