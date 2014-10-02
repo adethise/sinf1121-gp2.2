1b473f14adf8c4b1708a20e4c86317785bcb3b82
Charles Jacquet <charles.jacquet7@gmail.com>
2014-10-02 16:49:19 +0200

    pre/post

--- mission1/code/PSCalculator.java
+++ mission1/code/PSCalculator.java
@@ -8,6 +8,9 @@ public class PSCalculator {
 	private static Map<String, Float> def = new HashMap<String, Float>();
 	private static String outString = new String();	
 	private static int intLine = 1;
+	/*
+	 * @post: permet de récuperer la sortie de l'interpreteur
+	 */
 	public String getOutString() {
 		return outString;
 	}
@@ -15,7 +18,10 @@ public class PSCalculator {
 	public PSCalculator() {
 		PSStack = new DNodeStack();
 	}
-
+	/*
+	 * @Pré : Prend un String à interpreter
+	 * @Post: Interprete le language
+	 */
 	void feedStackFromLine(String lineToFeed){
 		String[] tabElement = lineToFeed.split(" ");
 		for( String element : tabElement)
@@ -43,7 +49,10 @@ public class PSCalculator {
 		return;
 	}
 	
-	
+	/*
+	 * @pre : prend un élément à interpreter
+	 * @post : l'envoie sur la bonne fonction
+	 */
 	private static int interpretor(String element)
 	{	
 		switch(element){
@@ -104,6 +113,10 @@ public class PSCalculator {
 		}
 	}
 
+	/*
+	 * @pre:un element à interpreter
+	 * @post: renvoie true si l'élément est un float, false dans le cas contraire.
+	 */
 	private static boolean isFloat(String element)
 	{
 
@@ -114,12 +127,18 @@ public class PSCalculator {
 		catch(NumberFormatException err){return false;} 
 
 	}
+	/*
+	 * @post: récupere un float de la stack
+	 */
 	private static Float getFloatFromStack()
 	{
 		Float returnFloat;
 			returnFloat = Float.parseFloat(PSStack.pop().toString());
 		return 	returnFloat;	
 	}
+	/*
+	 * @post: effectue l'opération d'addition de deux float contenu dans la stack et met la réponse sur la stack
+	 */
 	private static void add()
 	{
 		Float temp1 = getFloatFromStack();
@@ -130,6 +149,9 @@ public class PSCalculator {
 			PSStack.push(temp1 + temp2);
 
 	}
+	/*
+	 * @post: effectue l'opération de substraction de deux float contenu dans la stack et met la réponse sur la stack
+	 */
 	private static void sub()
 	{
 		Float temp1 = getFloatFromStack();
@@ -140,6 +162,9 @@ public class PSCalculator {
 			PSStack.push(temp2 - temp1);
 
 	}
+	/*
+	 * @post: effectue l'opération de multiplication de deux float contenu dans la stacket met la réponse sur la stack
+	 */
 	private static void mul()
 	{
 		Float temp1 = getFloatFromStack();
@@ -150,6 +175,9 @@ public class PSCalculator {
 			PSStack.push(temp1 * temp2);
 
 	}
+	/*
+	 * @post: effectue l'opération de division de deux float contenu dans la stack et met la réponse sur la stack
+	 */
 	private static void div()
 	{
 		Float temp1 = getFloatFromStack();
@@ -160,6 +188,12 @@ public class PSCalculator {
 			PSStack.push(temp2 / temp1);
 
 	}
+	/*
+	 * @pre: prend un booléan pour différencier la fonction equal et not equal
+	 * @post renvoie un booléan 
+	 * 	si not est false : renvoie true si les éléments sont égaux
+	 *  si not est true : renvoie true si les éléments sont différent
+	 */
 	private static void eq(boolean not)
 	{
 		Float temp1 = getFloatFromStack();
@@ -182,6 +216,9 @@ public class PSCalculator {
 		}
 		PSStack.push(tempResult);
 	}
+	/*
+	 * @post : prend les 2 derniers élement de la stack et les interprete de façon à définir une variable
+	 */
 	private static void def()
 	{
 		Float fl = getFloatFromStack();
@@ -200,12 +237,18 @@ public class PSCalculator {
 		def.put(d, fl);
 	}
 
-
+	/*
+	 * @post: effectue l'opération de duplication du dernier élément contenu dans la stack 
+	 * 
+	 */ 
 	private static void dup()
 	{
 			PSStack.push(PSStack.top());
 
 	}
+	/*
+	 * @post: effectue l'opération d'interchanger les 2 derniers élément de la pile
+	 */
 	private static void exch()
 	{
 		Object temp1 = PSStack.pop();
@@ -213,10 +256,16 @@ public class PSCalculator {
 		PSStack.push(temp2);
 		PSStack.push(temp1);
 	}
+	/*
+	 * @post: enleve un élément de la stack
+	 */
 	private static void pop()
 	{
 			PSStack.pop();
 	}
+	/*
+	 * @post : affiche/écrit le résultat
+	 */
 	private static void pstack()
 	{
 		outString += (intLine++) + ") "+PSStack.toString() + '\n';
--- mission1/code/PSCalculatorAPP.java
+++ mission1/code/PSCalculatorAPP.java
@@ -3,7 +3,12 @@ import java.util.List;
 
 
 public class PSCalculatorAPP {
-
+	/*
+	 * @pre: 2 arguments, le premier est le chemin d'accès du fichier qu'il faut lire.
+	 * 		 Le second est le nom du fichier dans lequel il faut écrire les résultats.
+	 * @post: prends les insctructions et valeurs dans le fichier 1, interprete le language Post-script
+	 * 		  et ensuite écrit le résultat dans un fichier.
+	 */
 	public static void main(String[] args) {		
 		
 		List<String> file = new ArrayList<String>(); 
