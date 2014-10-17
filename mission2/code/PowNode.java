d2af35960215d346900a233c4855d12c4765992f
Arnaud Dethise <arnaud.dethise@student.uclouvain.be>
2014-10-17 17:44:24 +0200

    fix power derivation

--- mission2/code/PowNode.java
+++ mission2/code/PowNode.java
@@ -1,4 +1,7 @@
-
+/*
+ * This is the node for the power function.
+ * Derivation will not work if b is not a constant.
+ */
 public class PowNode implements OperationNode {
 
 	private OperationNode a,b; // operands
@@ -16,7 +19,7 @@ public class PowNode implements OperationNode {
 
 	public OperationNode derivate()
 	{
-		return new MulNode(a.derivate(),new PowNode(a,new SubNode(b,new ConstNode(1))));
+		return new MulNode(a.derivate(), new MulNode(b, new PowNode(a,new SubNode(b,new ConstNode(1)))));
 	}
 
 	public String toString()
