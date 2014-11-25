5cdb6b085aaa33b81a4b281e5841d6869c59a274
Arnaud Dethise <arnaud.dethise@student.uclouvain.be>
2014-11-25 17:27:25 +0100

    skel compress

---
+++ mission5/code/Compress.java
@@ -0,0 +1,37 @@
+/**
+ *
+ */
+public class Compress
+{
+
+	/*
+	 *
+	 */
+	public static void main(String[] args)
+	{}
+
+	/**
+	 * Help
+	 */
+	public static void usage()
+	{}
+
+	/**
+	 *
+	 */
+	public static void writeHeader(OutputBitStream out, Set<Map.Entry<Character,String>> set, int filelen, int version) throws IOException
+	{}
+
+	/**
+	 *
+	 */
+	public static void writeData(OutputBitStream out, Reader in, Map map) throws IOException
+	{}
+
+	/**
+	 *
+	 */
+	public static Set<Map.Entry<Character,Integer>> computeFrequency(Reader in)
+	{}
+
+}
