24763a91d058ea07fe3fb3e0c023b662f9a8735b
rhenneton <romain.henneton@hotmail.fr>
2014-12-10 17:20:02 +0100

    ajout

--- mission6/code/Graph.java
+++ mission6/code/Graph.java
@@ -1,17 +1,17 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
+import java.io.*;
 
 public class Graph
 {
 	private List<List<Edge>> main;
-	private nnodes;
+	private int nnodes;
 
 	//Edge = arete
 	//Vertex = Sommet
 	public class Edge
 	{
-		private int cost;
-		private int origin, destination;
+		public int cost;
+		public int origin, destination;
 
 		public Edge(int cost, int origin, int destination)
 		{
@@ -23,10 +23,43 @@ public class Graph
 
 	public Graph(String filename, int n)
 	{
-		main = new ArrayList(n);
+		String line = null;
+		String [] tab =null;
+		
+		main = new ArrayList<List<Edge>>(n);
 		for (int i = 0 ; i < n ; i++ ) {
-			main.add(i, new LinkedList());
+			main.add(i, new LinkedList<Edge>());
 		}
 		this.nnodes = n;
+		
+		try
+		{
+			BufferedReader buff = new BufferedReader(new FileReader(filename));
+			while ((line = buff.readLine())!=null)
+			{
+				tab = line.split("\t");
+				Edge edge = new Edge( Integer.parseInt(tab[2]),Integer.parseInt(tab[0]),Integer.parseInt(tab[1]));
+				main.get(edge.origin).add(edge);
+				main.get(edge.destination).add(edge);
+			}
+	
+
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace(System.err);
+		}
+	}
+	
+	public List<Edge> getNode(int number)
+	{
+		return main.get(number);
+	}
+	
+	
+	public static void main(String[]args)
+	{
+		System.out.println("Kikooooooo :) :) :) :)");
 	}
 }
+
